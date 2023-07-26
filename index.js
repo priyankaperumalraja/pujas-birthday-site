120d5065b2483f021f3765154bb3c4e2818f67cd
priyankaperumalraja <130401934+priyankaperumalraja@users.noreply.github.com>
2023-07-25 20:10:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var clickNum = 0;
+
+// $("a").click(function() {
+//     var audio = new Audio("sounds/mixkit-hard-typewriter-click-1119.wav");
+//     audio.play();
+
+    // clickNum++;
+    // changeLocation();
+// })
+
+
+// function changeLocation(){
+//     if (clickNum === 1) {
+//         // $(".div5").html("");
+//         // $(".div1").html("<button id='button'>Click Here</button>").addClass("centering");
+        
+//     }
+//     else if (clickNum === 2){
+//         $(".div1").html("");
+//         $(".div3").html("<button id='button'>Click Here</button>").addClass("centering");
+//     }
+//     else if (clickNum === 3) {
+
+//     }
+//     else if (clickNum === 4) {
+
+//     }
+//     else {
+
+//     }
+//     console.log(clickNum);
+    
+// }
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,8 @@
+// $("#cake").on("mouseenter", function(){
+//     $("video").muted = false;
+// });
+
+
+var audio = new Audio("sounds/happy-birthday-my-kitty-120918.mp3");
+audio.play();
+
